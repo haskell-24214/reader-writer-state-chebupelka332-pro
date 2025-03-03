5d562ce519af9d82be2a86bff3a34f14d3372e39
chebupelka332-pro <uchebmaksimtok2006@yandex.ru>
2025-03-03 19:30:35 +0700

    Done task for State

--- src/Tier2/State.hs
+++ src/Tier2/State.hs
@@ -9,43 +9,50 @@ emptyRegisters = Registers 0 0 False 0
 type Calculation = State Registers Int
 
 plus :: Calculation
-plus = undefined
+plus = state $ \(Registers a b _ _) -> (a + b, Registers a b False (a + b))
 
 minus :: Calculation
-minus = undefined
+minus = state $ \(Registers a b _ _) -> (a - b, Registers a b False (a - b))
 
 productS :: Calculation
-productS = undefined
+productS = state $ \(Registers a b _ _) -> (a * b, Registers a b False (a * b))
 
-div :: Calculation
-div = undefined
+divide :: Calculation
+divide = state $ \(Registers a b _ _) -> if b == 0
+                                        then (0, emptyRegisters)
+                                        else (a `Prelude.div` b, Registers a b False (a `Prelude.div` b))
 
 swap :: Calculation
-swap = undefined
+swap = state $ \(Registers a b bl acc) -> (acc, Registers b a bl acc)
 
 blinkS :: Calculation
-blinkS = undefined
+blinkS = state $ \(Registers a b bl acc) -> (acc, Registers a b (not bl) acc)
 
 accS :: Calculation
-accS = undefined
+accS = state $ \(Registers a b bl acc) -> if not bl
+                                         then (acc, Registers acc b (not bl) acc)
+                                         else (acc, Registers a acc (not bl) acc)
 
 number :: Int -> Calculation
-number x = undefined
+number x = state $ \(Registers a b bl acc) -> if not bl
+                                             then (x, Registers x b (not bl) acc)
+                                             else (x, Registers a x (not bl) acc)
 
 commandToCalculation :: String -> Calculation
 commandToCalculation s =
   case s of
-    "+" -> plus
-    "-" -> minus
-    "*" -> productS
+    "+"    -> plus
+    "-"    -> minus
+    "*"    -> productS
+    "/"    -> divide
     "swap" -> swap
-    "blink" -> blinkS
-    "acc" -> accS
-    x -> number (read x)
+    "blink"-> blinkS
+    "acc"  -> accS
+    x      -> number (read x)
 
 buildCalculation :: [String] -> Calculation
-buildCalculation xs = 
-  foldl (\a x -> a >>= (\_ -> x)) (state (\s -> (0, s))) (map commandToCalculation xs)
+buildCalculation xs =
+  foldl (\a x -> a >>= (\_ -> commandToCalculation x)) (return 0) xs
 
 calculate :: [String] -> Int
 calculate xs = evalState (buildCalculation xs) emptyRegisters
